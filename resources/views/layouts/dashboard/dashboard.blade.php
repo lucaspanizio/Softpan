30d5611805c9ebe50db9c33812aeffb0f4234d89
lucaspanizio <lucas_panizio@hotmail.com>
2019-11-21 18:29:59 -0300

    Dashboard cols

--- resources/views/layouts/dashboard/dashboard.blade.php
+++ resources/views/layouts/dashboard/dashboard.blade.php
@@ -7,15 +7,15 @@
     <div class="section__content section__content--p30">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-8 col-md">
+                <div class="col-lg-12 col-md">
                     <div class="au-card m-b-20">
                         <div class="au-card-inner">
-                            <div class="chartjs-size-monitor" >
-                                <div class="chartjs-size-monitor-expand" >
-                                    <div ></div>
+                            <div class="chartjs-size-monitor">
+                                <div class="chartjs-size-monitor-expand">
+                                    <div></div>
                                 </div>
-                                <div class="chartjs-size-monitor-shrink" >
-                                    <div ></div>
+                                <div class="chartjs-size-monitor-shrink">
+                                    <div></div>
                                 </div>
                             </div>
                             <h3 class="title-2 m-b-40">Receita x Despesas (Mensal)</h3>
@@ -24,31 +24,31 @@
                     </div>
 
                 </div>
+            </div>
 
-                <div class="col-lg-3">
-                    <div class="row">
-                        <div class="col-md-4 col-sm-6 col-lg">
-                            <div class="statistic__item">
-                                <span class="desc"><b>Receitas (Anual)</b></span>
-                                <h2 class="number" style="color:green">10,368</h2>
-                                <div class="icon">
-                                    <i class="zmdi zmdi-money"></i>
-                                </div>
-                            </div>
+            {{-- <div class="col-lg-3"> --}}
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-lg">
+                    <div class="statistic__item">
+                        <h3 class="title-2 m-b-50">Receitas (Anual)</h3>
+                        <h2 class="number" style="color:green">R$ 10.368,00</h2>
+                        <div class="icon">
+                            <i class="zmdi zmdi-money"></i>
                         </div>
+                    </div>
+                </div>
 
-                        <div class="col-md-4 col-sm-6 col-lg">
-                            <div class="statistic__item">
-                                <span class="desc"><b>Despesas (Anual)</b></span>
-                                <h2 class="number" style="color:red">388,688</h2>
-                                <div class="icon">
-                                    <i class="zmdi zmdi-money-off"></i>
-                                </div>
-                            </div>
+                <div class="col-md-4 col-sm-6 col-lg">
+                    <div class="statistic__item">
+                        <h3 class="title-2 m-b-50">Despesas (Anual)</h3>
+                        <h2 class="number" style="color:red">R$ 388,68</h2>
+                        <div class="icon">
+                            <i class="zmdi zmdi-money-off"></i>
                         </div>
                     </div>
                 </div>
             </div>
+            {{-- </div> --}}
         </div>
     </div>
 </div>
